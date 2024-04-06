38f6b055a46553dc07e11565c9e6eccf877529e5
Mari0130 <141742371+Marii0130@users.noreply.github.com>
2024-04-06 00:36:34 -0300

    Parte B ejercicio 1 y 2

--- Practico 1/Parte B/Ejercicio 1/ejercicio-1.cpp
+++ Practico 1/Parte B/Ejercicio 1/ejercicio-1.cpp
@@ -1,5 +1,44 @@
 #include<iostream>
 using namespace std;
 int main(){
+	int num;
+	cout << "Las tablas de multiplicacion" << endl;
+	cout << "Ingrese un numero del 1 al 10 para acceder a las opciones del menu" << endl;
+	cout <<"1) Ver tabla del 1" << endl;
+	cout <<"2) Ver tabla del 2" << endl;
+	cout <<"3) Ver tabla del 3" << endl;
+	cout <<"4) Ver tabla del 4" << endl;
+	cout <<"5) Ver tabla del 5" << endl;
+	cout <<"6) Ver tabla del 6" << endl;
+	cout <<"7) Ver tabla del 7" << endl;
+	cout <<"8) Ver tabla del 8" << endl;
+	cout <<"9) Ver tabla del 9" << endl;
+	cout <<"10) Ver tabla del 10" << endl;
+	cout << endl;
+	cout << "Opcion a escoger: ";
+	cin >> num;
+	switch(num){
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+			cout << endl;
+			cout << "Tabla del " << num << endl;
+			cout << endl;
+			for(int i = 1; i <= 10; i++){
+				cout << num << " x " << i << " = " << (num*i) << endl;
+			}
+			break;
+		default:
+			cout << "ERROR, numero no valido.";
+			break;
+	}
+	
 	return 0;
 }
--- Practico 1/Parte B/Ejercicio 2/ejercicio-2.cpp
+++ Practico 1/Parte B/Ejercicio 2/ejercicio-2.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
+	int num, contador = 0;
+	cout << "Contador de numeros positivos" << endl;
+	cout << endl;
+	do{
+		cout << "Ingrese un numero: ";
+		cin >> num;
+		if (num > 0){
+			contador++;
+		}
+	}while(num != 0);
+	cout << endl;
+	cout << "Acaba de salir del bucle." << endl;
+	cout << "La cantidad de numeros ingresado mayores a cero es de " << contador << " numeros." << endl;
+	
 	return 0;
 }

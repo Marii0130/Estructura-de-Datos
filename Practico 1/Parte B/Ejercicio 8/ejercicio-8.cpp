466dc44b45c0ec9ed266fa5b0fa12ae085f9ddc1
Mari0130 <141742371+Marii0130@users.noreply.github.com>
2024-04-07 21:48:14 -0300

    pequeña modificacion

--- Practico 1/Parte B/Ejercicio 8/ejercicio-8.cpp
+++ Practico 1/Parte B/Ejercicio 8/ejercicio-8.cpp
@@ -1,8 +1,4 @@
 #include<iostream>
-/* En una clase de 5 alumnos se han realizado 3 exámenes, y se requiere determinar según se elija una opción
-el número de: a) Alumnos que aprobaron todos los exámenes. b) Alumnos que aprobaron al menos un examen. c)
-Alumnos que aprobaron únicamente el último examen.
-*/
 using namespace std;
 int main(){
 	int alumnos[5][3], alumno[3], nota, op, cont1 = 0, cont2 = 0, cont3 = 0, aux1 = 0, aux2 = 0;
